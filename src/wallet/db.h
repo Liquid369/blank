fd259f3b9c5fd5eb21a6e3de79754433ca8c8353
Philip Kaufmann <phil.kaufmann@t-online.de>
2015-04-23 14:55:02 +0200

    remove unused classes from db.h

--- src/wallet/db.h
+++ src/wallet/db.h
@@ -20,10 +20,7 @@
 
 #include <db_cxx.h>
 
-class CDiskBlockIndex;
-class COutPoint;
-
-struct CBlockLocator;
+extern unsigned int nWalletDBUpdated;
 
 static const unsigned int DEFAULT_WALLET_DBLOGSIZE = 100;
 static const bool DEFAULT_WALLET_PRIVDB = true;
