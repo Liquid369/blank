9271ace157344ea4cbb3f742c70612a124eea7c0
Pieter Wuille <pieter.wuille@gmail.com>
2017-07-07 16:06:56 -0700

    Support serialization as another type without casting
    This adds a READWRITEAS(type, obj) macro which serializes obj as if it
    were casted to (const type&) when const, and to (type&) when non-const.
    
    This makes it usable in serialization code that uses a single
    implementation for both serialization and deserializing, which doesn't
    know the constness of the object involved.

--- src/script/script.h
+++ src/script/script.h
@@ -396,7 +396,7 @@ public:
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
-        READWRITE(static_cast<CScriptBase&>(*this));
+        READWRITEAS(CScriptBase, *this);
     }
 
     CScript& operator+=(const CScript& b)
