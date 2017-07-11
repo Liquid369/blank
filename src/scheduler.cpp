207c17f3fc7131f63cd7f3c94ff7ca4514b35d1c
Matt Corallo <git@bluematt.me>
2017-07-10 21:08:19 -0400

    Expose if CScheduler is being serviced, assert its not in EmptyQueue

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -121,6 +121,10 @@ size_t CScheduler::getQueueInfo(boost::chrono::system_clock::time_point &first,
     return result;
 }
 
+bool CScheduler::AreThreadsServicingQueue() const {
+    return nThreadsServicingQueue;
+}
+
 void SingleThreadedSchedulerClient::MaybeScheduleProcessQueue() {
     {
         LOCK(m_cs_callbacks_pending);
@@ -173,6 +177,7 @@ void SingleThreadedSchedulerClient::AddToProcessQueue(std::function<void (void)>
 }
 
 void SingleThreadedSchedulerClient::EmptyQueue() {
+    assert(!m_pscheduler->AreThreadsServicingQueue());
     bool should_continue = true;
     while (should_continue) {
         ProcessQueue();
