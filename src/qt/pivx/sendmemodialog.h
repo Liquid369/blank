708eb8d620bb3704d0f421f1e22f3d7dd6effbc6
furszy <matiasfurszyfer@protonmail.com>
2020-11-29 17:20:46 -0300

    GUI: send screen, implementing custom memo flow.

---
+++ src/qt/pivx/sendmemodialog.h
@@ -0,0 +1,44 @@
+// Copyright (c) 2019-2020 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#ifndef SENDMEMODIALOG_H
+#define SENDMEMODIALOG_H
+
+#include "qt/pivx/focuseddialog.h"
+
+class WalletModel;
+class SnackBar;
+
+namespace Ui {
+class SendMemoDialog;
+}
+
+class SendMemoDialog : public FocusedDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SendMemoDialog(QWidget* parent, WalletModel* model);
+    ~SendMemoDialog();
+
+    QString getMemo();
+    void setMemo(QString memo);
+    void showEvent(QShowEvent* event) override;
+
+public Q_SLOTS:
+    void textChanged();
+
+private:
+    WalletModel* walletModel;
+    Ui::SendMemoDialog *ui;
+    SnackBar *snackBar = nullptr;
+
+    void inform(const QString& text);
+
+private Q_SLOTS:
+    void reset();
+    void accept() override;
+};
+
+#endif // SENDMEMODIALOG_H
