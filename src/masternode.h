9bd413cc766be341f63c664a66b1c0881fa1203f
random-zebra <random.zebra@protonmail.com>
2020-11-30 04:55:10 +0100

    Refactor: Introduce CMasternodeMan::CheckSpentCollaterals

--- src/masternode.h
+++ src/masternode.h
@@ -230,6 +230,8 @@ public:
         return lastPing.IsNull() ? false : now - lastPing.sigTime < seconds;
     }
 
+    void SetSpent() { LOCK(cs); activeState = CMasternode::MASTERNODE_VIN_SPENT; }
+
     void Disable()
     {
         LOCK(cs);
