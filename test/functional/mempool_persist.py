3ace13b3210211b62939d90f00318f53c255c18c
furszy <matiasfurszyfer@protonmail.com>
2021-02-20 11:15:50 -0300

    qa: Fix some tests to work on native windows
    Coming from btc@fa3528a85b05ea9507077f3eb340c9fb189251a6

--- test/functional/mempool_persist.py
+++ test/functional/mempool_persist.py
@@ -59,7 +59,9 @@ class MempoolPersistTest(PivxTestFramework):
 
         self.log.debug("Stop-start node0 and node1. Verify that node0 has the transactions in its mempool and node1 does not.")
         self.stop_nodes()
-        self.start_node(1)  # Give this one a head-start, so we can be "extra-sure" that it didn't load anything later
+        # Give this node a head-start, so we can be "extra-sure" that it didn't load anything later
+        # Also don't store the mempool, to keep the datadir clean
+        self.start_node(1, extra_args=["-persistmempool=0"])
         self.start_node(0)
         self.start_node(2)
         # Give pivxd a second to reload the mempool
