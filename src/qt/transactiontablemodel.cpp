69b3330923ab0060cc8bb15b984ba52ce27a409f
furszy <matiasfurszyfer@protonmail.com>
2021-02-26 10:27:04 -0300

    GUI: txmodel, do not lock cs_wallet if no wtx status update is needed.

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -303,25 +303,21 @@ public:
     {
         if (idx >= 0 && idx < cachedWallet.size()) {
             TransactionRecord* rec = &cachedWallet[idx];
-
-            // Get required locks upfront. This avoids the GUI from getting
-            // stuck if the core is holding the locks for a longer time - for
-            // example, during a wallet rescan.
-            //
-            // If a status update is needed (blocks came in since last check),
-            //  update the status of this transaction from the wallet. Otherwise,
-            // simply re-use the cached status.
-            TRY_LOCK(wallet->cs_wallet, lockWallet);
-            if (lockWallet && rec->statusUpdateNeeded(cur_block_num)) {
-                std::map<uint256, CWalletTx>::iterator mi = wallet->mapWallet.find(rec->hash);
-
-                if (mi != wallet->mapWallet.end()) {
-                    rec->updateStatus(mi->second, cur_block_num);
+            if (!cur_block_hash.IsNull() && rec->statusUpdateNeeded(cur_block_num)) {
+                // If a status update is needed (blocks came in since last check),
+                // update the status of this transaction from the wallet. Otherwise,
+                // simply re-use the cached status.
+                TRY_LOCK(wallet->cs_wallet, lockWallet);
+                if (lockWallet) {
+                    auto mi = wallet->mapWallet.find(rec->hash);
+                    if (mi != wallet->mapWallet.end()) {
+                        rec->updateStatus(mi->second, cur_block_num);
+                    }
                 }
             }
             return rec;
         }
-        return 0;
+        return nullptr;
     }
 };
 
