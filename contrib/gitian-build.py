7eda846adb4320dfc7bd74fe6523cb2ec4e51b4e
Liquid369 <Liquid369@gmail.com>
2021-04-13 11:58:57 -0500

    Setup Gitian

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2018-2019 The Bitcoin Core developers
 # Copyright (c) 2019-2020 The PIVX Developers
-Copyright (c) 2020-2021 The Flits Developers
+# Copyright (c) 2020-2021 The Flits Developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -105,13 +105,13 @@ def setup_darwin():
 
 def setup_repos():
     if not os.path.isdir('gitian.sigs'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/fls-Project/gitian.sigs.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/dogecash/gitian.sigs.git'])
     if not os.path.isdir('fls-detached-sigs'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/fls-Project/fls-detached-sigs.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/dogecash/detached-sigs.git'])
     if not os.path.isdir('gitian-builder'):
         subprocess.check_call(['git', 'clone', 'https://github.com/devrandom/gitian-builder.git'])
     if not os.path.isdir('fls'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/fls-Project/fls.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/Liquid369/fls.git'])
     os.chdir('gitian-builder')
     make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
     if args.docker:
@@ -257,7 +257,7 @@ def main():
     parser = argparse.ArgumentParser(description='Script for running full Gitian builds.')
     parser.add_argument('-c', '--commit', action='store_true', dest='commit', help='Indicate that the version argument is for a commit or branch')
     parser.add_argument('-p', '--pull', action='store_true', dest='pull', help='Indicate that the version argument is the number of a github repository pull request')
-    parser.add_argument('-u', '--url', dest='url', default='https://github.com/fls-Project/fls', help='Specify the URL of the repository. Default is %(default)s')
+    parser.add_argument('-u', '--url', dest='url', default='https://github.com/Liquid369/fls.git', help='Specify the URL of the repository. Default is %(default)s')
     parser.add_argument('-v', '--verify', action='store_true', dest='verify', help='Verify the Gitian build')
     parser.add_argument('-b', '--build', action='store_true', dest='build', help='Do a Gitian build')
     parser.add_argument('-s', '--sign', action='store_true', dest='sign', help='Make signed binaries for Windows and MacOS')
