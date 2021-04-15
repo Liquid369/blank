f28d5a9578ad7d3456c3134b97f738f01fce5f28
Liquid369 <Liquid369@gmail.com>
2021-04-15 10:36:56 -0500

    fix again

--- src/test/addrman_tests.cpp
+++ src/test/addrman_tests.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2012-2015 The Bitcoin Core developers
 // Copyright (c) 2019 The PIVX Developers
-Copyright (c) 2019 The Flits Developers
+// Copyright (c) 2019 The Flits Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include "addrman.h"
