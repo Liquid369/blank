4c1ad3cbbe6edbe3e16e58c0b204c66925fc4bc3
Liquid369 <Liquid369@gmail.com>
2021-04-15 10:29:25 -0500

    Readjust

--- src/sapling/incrementalmerkletree.h
+++ src/sapling/incrementalmerkletree.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2016-2018 The Zcash developers
 // Copyright (c) 2020 The PIVX Developers
-# Copyright (c) 2020 The Flits Developers
+// Copyright (c) 2020 The Flits Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
--- src/test/txvalidationcache_tests.cpp
+++ src/test/txvalidationcache_tests.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2011-2015 The Bitcoin Core developers
 // Copyright (c) 2020 The PIVX Developers
-# Copyright (c) 2020 The Flits Developers
+// Copyright (c) 2020 The Flits Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
