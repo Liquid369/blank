cc3bc0cdd7eeef57d42b9e9ad7153ef2dd2467d9
Liquid369 <Liquid369@gmail.com>
2021-06-01 14:44:34 -0500

    [GUI] Hover Colors, Backgrounds
    [GUI] More styling
    
    [GUI] More Styling

--- src/qt/dogecash/dashboardwidget.cpp
+++ src/qt/dogecash/dashboardwidget.cpp
@@ -467,7 +467,7 @@ void DashboardWidget::changeChartColors()
         gridLineColorX = QColor(255,255,255);
         linePenColorY = gridLineColorX;
         backgroundColor = linePenColorY;
-        axisY->setGridLineColor(QColor("#1a000000"));
+        axisY->setGridLineColor(QColor("#000000"));
     } else {
         gridY = QColor("#40ffffff");
         axisY->setGridLineColor(gridY);
