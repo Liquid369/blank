210466987041042ac4258855341e884abe78356c
Liquid369 <Liquid369@gmail.com>
2021-05-31 19:32:12 -0500

    Remove biplist import

--- contrib/macdeploy/custom_dsstore.py
+++ contrib/macdeploy/custom_dsstore.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2013-2017 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-import biplist
 from ds_store import DSStore
 from mac_alias import Alias
 import sys
