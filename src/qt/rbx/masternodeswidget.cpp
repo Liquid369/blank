f0b76256e75fdd886bba6f709c794917a760bd34
Liquid <Liquid369@gmail.com>
2021-07-14 13:53:30 -0500

    MN Widget

--- src/qt/rbx/masternodeswidget.cpp
+++ src/qt/rbx/masternodeswidget.cpp
@@ -478,7 +478,7 @@ void MasterNodesWidget::onCreateMNClicked()
         return;
     }
 
-    if (walletModel->getBalance() <= (COIN * 10000)) {
+    if (walletModel->getBalance() <= (COIN * 2500)) {
         inform(tr("Not enough balance to create a masternode, 2,500 %1 required.").arg(CURRENCY_UNIT.c_str()));
         return;
     }
