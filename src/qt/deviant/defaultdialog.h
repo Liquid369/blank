a06c5c595425815627bbeac8261bb084e5ce8cc9
Liquid <Liquid369@gmail.com>
2021-10-23 11:47:47 -0500

    More file renames and adjustments

---
+++ src/qt/deviant/defaultdialog.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2017-2020 The PIVX Developers
+// Copyright (c) 2020 The Deviant Developers
+
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DEFAULTDIALOG_H
+#define DEFAULTDIALOG_H
+
+#include "qt/deviant/focuseddialog.h"
+
+namespace Ui {
+class DefaultDialog;
+}
+
+class DefaultDialog : public FocusedDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DefaultDialog(QWidget *parent = nullptr);
+    ~DefaultDialog();
+
+    void setText(const QString& title = "", const QString& message = "", const QString& okBtnText = "", const QString& cancelBtnText = "");
+
+    bool isOk = false;
+
+public Q_SLOTS:
+    void accept() override;
+
+private:
+    Ui::DefaultDialog *ui;
+};
+
+#endif // DEFAULTDIALOG_H
---
+++ src/qt/deviant/mnrow.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2017-2020 The PIVX Developers
+// Copyright (c) 2020 The Deviant Developers
+
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "qt/deviant/mnrow.h"
+#include "qt/deviant/forms/ui_mnrow.h"
+#include "qt/deviant/qtutils.h"
+
+MNRow::MNRow(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MNRow)
+{
+    ui->setupUi(this);
+    setCssProperty(ui->labelAddress, "text-list-body2");
+    setCssProperty(ui->labelName, "text-list-title1");
+    setCssProperty(ui->labelDate, "text-list-caption-medium");
+    ui->lblDivisory->setStyleSheet("background-color:#bababa;");
+}
+
+void MNRow::updateView(QString address, QString label, QString status, bool wasCollateralAccepted)
+{
+    ui->labelName->setText(label);
+    ui->labelAddress->setText(address);
+    if (!wasCollateralAccepted) status = tr("Collateral tx not found");
+    ui->labelDate->setText(tr("Status: %1").arg(status));
+}
+
+MNRow::~MNRow()
+{
+    delete ui;
+}
