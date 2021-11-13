02de3a2b7af912d3a7d539acb7527534899babed
Liquid <Liquid369@gmail.com>
2021-11-13 06:14:54 -0600

    Adjust block value checks

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -235,7 +235,7 @@ bool IsBlockValueValid(int nHeight, CAmount& nExpectedValue, CAmount nMinted)
             }
         }
     }
-    if ((nHeight == 609500) || (nHeight <= 1760000)) {
+    if ((nHeight == 609500) || (nHeight <= 1780000)) {
         return true;
     }
     return nMinted <= nExpectedValue;
